1efd5a8ba6031b04543aa1c980373b5ad9f8088f
papistan <jpapisan@gmail.com>
2017-06-24 16:01:24 -0700

    Add relative model

--- db/migrate/20170624225725_create_relatives.rb
+++ db/migrate/20170624225725_create_relatives.rb
@@ -1,7 +1,9 @@
 class CreateRelatives < ActiveRecord::Migration[5.1]
   def change
     create_table :relatives do |t|
-
+    	t.string :full_name
+    	t.string :phone
+    	
       t.timestamps
     end
   end
