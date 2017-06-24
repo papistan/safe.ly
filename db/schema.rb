bc6b41549d61774c5a6fb7a769f15ae2b30587e6
papistan <jpapisan@gmail.com>
2017-06-24 15:54:38 -0700

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624211935) do
+ActiveRecord::Schema.define(version: 20170624224911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170624211935) do
     t.string "location"
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
