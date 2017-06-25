685568fc12d8a63cf61247decc8f75872f2edbf2
papistan <jpapisan@gmail.com>
2017-06-24 23:43:16 -0700

    update forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to @user
+			redirect_to '/relatives/new'
+			# redirect_to @user
 		else
 			render 'new'
 		end
---
+++ public/ajax.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    $(".relative-section").on("submit", ".relative-form", function(e){
+    e.preventDefault();
+    console.log(this);
+    
+    // $form = $(this);
+  })
+})
